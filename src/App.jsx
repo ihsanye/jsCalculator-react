261623fcdbe7c964be87d3728785607233fc08a2
ihsanye <ihsany98@gmail.com>
2024-08-29 20:42:04 +0300

    all done - fix expressions later

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,83 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
-  const buttonPress = (symbol) => {
+  const [answer, setAnswer] = useState("");
+  const [expression, setExpression] = useState("");
+  const et = expression.trim();
 
+  const isOperator = (symbol) => {
+    return /[-+*/]/.test(symbol)
   }
+
+  const buttonPress = (symbol) => {
+    if (symbol === "clear") {
+      setAnswer("");
+      setExpression("0");
+      return;
+    } else if (symbol === "negative") {
+      if (answer === "") return;
+      setAnswer(answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer);
+    } else if (symbol === "percentage") {
+      if (answer === "") return;
+      setAnswer((parseFloat(answer) / 100).toString());
+    } else if (isOperator(symbol)) {
+      setExpression(et + " " + symbol + " ");
+    } else if (symbol === "=") {
+      calculate();
+    } else if (symbol === "0") {
+      if (expression.charAt(0) !== "0") {
+        setExpression(expression + symbol);
+      }
+    } else if (symbol === ".") {
+      const lastNumber = expression.split(/[-+*/]/g).pop();
+      if (lastNumber?.includes(".")) return;
+      setExpression(expression + symbol);
+    } else {
+      if (expression.charAt(0) === "0") {
+        setExpression(expression.slice(1) + symbol);
+      } else {
+        setExpression(expression + symbol);
+      }
+    }
+  };
+
+  const calculate = () => {
+    if (isOperator(et.charAt(et.length - 1))) return;
+    const parts = et.split(" ");
+    const newParts = [];
+    for (let i = parts.length - 1; i >= 0; i--) {
+      if (["*", "/", "+"].includes(parts[i]) && isOperator(parts[i - 1])) {
+        newParts.unshift(parts[i]);
+        let j = 0;
+        let k = i - 1;
+        while (isOperator(parts[k])) {
+          k--;
+          j++
+        }
+        i -= j;
+      } else {
+        newParts.unshift(parts[i]);
+      }
+    }
+    const newExpression = newParts.join(" ");
+    if (isOperator(newExpression.charAt(0))) {
+      setAnswer(eval(answer + newExpression));
+    } else {
+      setAnswer(eval(newExpression));
+    }
+    setExpression("");
+  };
 
   return (
     <div className="container">
       <h1>Calculator App</h1>
       <div className="calculator">
-        <div className="display">
-          <div className="answer">0</div>
-          <div className="expression">symbol</div>
+        <h2 id='brand' >CASIO</h2>
+        <div id="display">
+          <div id="answer">{answer}</div>
+          <div id="expression">{expression}</div>
         </div>
         <button id="clear" onClick={() => buttonPress("clear")}>AC</button>
         <button id="negative" onClick={() => buttonPress("negative")}>+/-</button>
@@ -25,7 +90,7 @@ function App() {
         <button id="four" onClick={() => buttonPress("4")}>4</button>
         <button id="five" onClick={() => buttonPress("5")}>5</button>
         <button id="six" onClick={() => buttonPress("6")}>6</button>
-        <button id="substract" onClick={() => buttonPress("-")}>–</button>
+        <button id="subtract" onClick={() => buttonPress("-")}>–</button>
         <button id="one" onClick={() => buttonPress("1")}>1</button>
         <button id="two" onClick={() => buttonPress("2")}>2</button>
         <button id="three" onClick={() => buttonPress("3")}>3</button>
